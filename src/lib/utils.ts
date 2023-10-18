b64b35c02072b4a0dcad6bd3d8fcb1b941e2305a
Abhishek <abhitiku2003@gmail.com>
2023-10-18 19:36:43 +0530

    some home setup

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,6 @@
-import { twMerge } from "tailwind-merge";
-import clsx, { ClassValue } from "clsx";
-
-export function cn(...inputs: ClassValue[]){
-    //twMerge to merge redundant tailwind classes and clsx to remove conflicts
-    return twMerge(clsx(inputs))
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+ 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
