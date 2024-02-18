24ee92e80affd9f06385e10c378ac32a2920cdc5
Abhishek <abhitiku2003@gmail.com>
2024-02-18 15:12:40 +0530

    added uploadthing

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -1,3 +1,5 @@
+import ChatWrapper from "@/components/ChatWrapper";
+import PdfRenderer from "@/components/PdfRenderer";
 import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { notFound, redirect } from "next/navigation";
@@ -24,7 +26,21 @@ const FileChat = async ({ params }: PageProps) => {
   });
   if (!file) notFound();
 
-  return <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]"></div>;
+  return (
+    <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
+      <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
+        {/* left side */}
+        <div className="flex-1 xl:flex">
+          <div className="px-4 py-6 sm:px-6 lg:ol-8 xl:flex-1 xl:px-6">
+            <PdfRenderer />
+          </div>
+        </div>
+        <div className="shrink-0 flex-[0.7] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
+          <ChatWrapper />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FileChat;
